4c5301149d4814d4b0a7ce2c70ac127171d7304f
Maxwell Lasky <mlasky46@gmail.com>
2021-04-04 09:47:38 -0600

    Set up deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/allstarwindow",
   siteMetadata: {
     siteUrl: `https://www.allstarwindowcleaning.com`,
     title: ``,
@@ -33,4 +34,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-};
+}
